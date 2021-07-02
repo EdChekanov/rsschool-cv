7d78061b7a3d282c49cf62d9f87f87b60dc9d6a4
Ed Chekanov <kide.da@gmail.com>
2021-07-02 14:31:55 +0300

    feat: add js - Scroll

---
+++ script/main.js
@@ -0,0 +1,18 @@
+let scrollLinks = document.querySelectorAll('.scroll');
+scrollLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = link.getAttribute('href').substring(1);
+        const el = document.getElementById(id);
+        const offset = 100;
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elRect = el.getBoundingClientRect().top;
+        const elPosition = elRect - bodyRect;
+        const offsetPosition = elPosition - offset;
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+
+    })
+});
