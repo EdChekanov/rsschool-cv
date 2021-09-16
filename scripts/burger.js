683fc6ec6406292a4a163fec1721fddd1bd810f0
Ed Chekanov <kide.da@gmail.com>
2021-09-16 11:58:22 +0300

    fix: burger problem

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,9 +1,13 @@
 (function () {
   const nav = document.querySelector('.nav');
 <<<<<<< HEAD
+<<<<<<< HEAD
   const main = document.querySelector('main');
 =======
 >>>>>>> 06a5d2a (feat: finish burger menu)
+=======
+  const main = document.querySelector('main');
+>>>>>>> c1703a6 (fix: burger problem)
   const burgerBtn = document.querySelector('.burger_btn');
 
   let onBurgerBtn = () => {
@@ -12,6 +16,9 @@
       nav.style.top = '0';
       nav.style.left = '0';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> c1703a6 (fix: burger problem)
       main.addEventListener('click', onBurgerBtn);
       main.addEventListener('touchstart', onBurgerBtn);
     } else {
@@ -19,11 +26,14 @@
       nav.style.left = '-250px';
       main.removeEventListener('click', onBurgerBtn);
       main.removeEventListener('touchstart', onBurgerBtn);
+<<<<<<< HEAD
 =======
     } else {
       nav.style.top = '-250px';
       nav.style.left = '-250px';
 >>>>>>> 06a5d2a (feat: finish burger menu)
+=======
+>>>>>>> c1703a6 (fix: burger problem)
     }
   };
 
