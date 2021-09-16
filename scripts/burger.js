92826a48a65849203e9e8b101b9e02c07be5f10c
Ed Chekanov <kide.da@gmail.com>
2021-09-16 11:58:22 +0300

    fix: burger problem

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,5 +1,6 @@
 (function () {
   const nav = document.querySelector('.nav');
+  const main = document.querySelector('main');
   const burgerBtn = document.querySelector('.burger_btn');
 
   let onBurgerBtn = () => {
@@ -7,9 +8,13 @@
     if (burgerBtn.classList.contains('close_burger')) {
       nav.style.top = '0';
       nav.style.left = '0';
+      main.addEventListener('click', onBurgerBtn);
+      main.addEventListener('touchstart', onBurgerBtn);
     } else {
       nav.style.top = '-250px';
       nav.style.left = '-250px';
+      main.removeEventListener('click', onBurgerBtn);
+      main.removeEventListener('touchstart', onBurgerBtn);
     }
   };
 
